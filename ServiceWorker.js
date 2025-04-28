8d7231fcbc581e69f65bcaf26d0da2a13ba4b603
alvinwiguna <alvinwiguna@yahoo.com>
2025-04-28 22:02:58 +0700

    fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-L_L App-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
     "Build/2bb1d5231f3e882a3b07f33db1403db7.framework.js",
-    "Build/825c7b2015956c48d55a5662a01a5fc4.data",
+    "Build/9ec7a071039e4b3e0a9c602f9f3c3b10.data",
     "Build/627d4c1db76e1ccc8bbae5b5aca64ebd.wasm",
     "TemplateData/style.css"
 
